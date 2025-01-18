5da8dc38f2d2a7966ea72fffbed5793c60eae329
Saklen12 <manjiresaklen2@gmail.com>
2025-01-18 22:20:14 +0530

    Create week1_exp1_307.cpp

---
+++ week1_exp1_307.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"Enter 1st Number:";
+    cin>>num1;
+    cout<<"Enter 2nd Number:";
+    cin>>num2;
+    cout<<"Addition of "<<num1<<"+"<<num2<<"="<<num1+num2<<"\n";
+    cout<<"Substraction of "<<num1<<"-"<<num2<<"="<<num1-num2<<"\n";
+    cout<<"Multiplication of "<<num1<<"*"<<num2<<"="<<num1*num2<<"\n";
+    cout<<"Division of "<<num1<<"/"<<num2<<"="<<num1/num2;
+    return 0;
+}
